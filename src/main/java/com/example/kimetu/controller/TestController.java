c04001188ea7bcb43181ddc5948f43bbca3d7478
setos <setos@ANZEN7>
2026-09-25 17:09:35 +0900

    test

---
+++ src/main/java/com/example/kimetu/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.example.kimetu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+	
+	@GetMapping("/")
+	public String testView() {
+		return "test";
+	}
+}
